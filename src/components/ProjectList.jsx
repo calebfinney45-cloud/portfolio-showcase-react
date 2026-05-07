f987840c18859f3ab5f272e9ef757d7a183bb040
Caleb Finney <calebfinney45@gmail.com>
2026-05-07 23:02:48 +0300

    feat:Update grid logic in ProjectList

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -11,7 +11,7 @@ export default function ProjectList({projects}) {
     }
 
     return(
-        <div className="grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {projects.map((project) => (
                 <ProjectCard key={project.id} project={project} />
             ))}
