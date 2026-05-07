a7b0fc5324446c9c5fc8a8ffcd6296baf6278657
Caleb Finney <calebfinney45@gmail.com>
2026-05-07 22:17:02 +0300

    Add tailwind and shadcn, complete ProjectCard.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import ProjectList from "@/components/ProjectList";
+import ProjectCard from "@/components/ProjectCard";
+
+export default function App() {
+  
+}
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,21 @@
+import { Card, CardHeader, CardTitle, CardContent} from "@/components/ui/card";
+
+export default function ProjectCard() {
+    return(
+        <Card className={"bg-white border-zinc-200"} >
+            <CardHeader>
+                <div className="flex justify-between items-center">
+                    <CardTitle>
+                        {project.name}
+                    </CardTitle>
+                    <span className="text-[10px] font-bold uppercase bg bg-zinc-100 px-2 py rounded text-zinc-600">
+                        {project.title}
+                    </span>
+                </div>
+            </CardHeader>
+            <CardContent>
+                <p className="text-sm text-zinc-500">{project.description}</p>
+            </CardContent>
+        </Card>
+    );
+}
