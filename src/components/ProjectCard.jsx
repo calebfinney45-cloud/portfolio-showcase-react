1a1c5455dbe9e9bd498bcc5818825186ac6c58a9
Caleb Finney <calebfinney45@gmail.com>
2026-05-08 19:38:52 +0300

    Add comments and spaces to ProjectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,13 +3,16 @@ import { Trash2, CircleChevronUp, CircleChevronDown } from "lucide-react";
 import { Button } from "./ui/button";
 import { useState } from "react";
 
+//Handles display for a single project card
 export default function ProjectCard({ project, onDelete }) {
+  //Local state to toggle description
   const [isOpen, setIsopen] = useState(false);
 
   return (
     <div className="py-8">
       <Card className={"bg-white border-zinc-200 overflow-hidden"}>
         <div className="w-full h-48 bg-zinc-100">
+          {/*Image logic: Uses project URL/placeholder. Includes onError to catch broken links which may mess up UI*/}
           <img
             src={
               project.image ||
@@ -22,13 +25,17 @@ export default function ProjectCard({ project, onDelete }) {
             }}
           />
         </div>
+
         <CardHeader>
           <div className="flex justify-between items-center">
             <CardTitle className="font-bold text-xl">{project.title}</CardTitle>
+
             <span className="text-[10px] font-bold uppercase bg-zinc-100 px-2 py rounded text-zinc-600">
               {project.category}
             </span>
+
             <div className="flex gap-1">
+              {/* Toggle logic: Clicking the Chevron icon flips 'isOpen'. Description only renders if isOpen = true */}
               <Button
                 variant="ghost"
                 size="icon"
@@ -41,11 +48,13 @@ export default function ProjectCard({ project, onDelete }) {
                 )}
               </Button>
             </div>
+
             <Button variant="ghost" size="icon" onClick={onDelete}>
               <Trash2 size={"16"} />
             </Button>
           </div>
         </CardHeader>
+
         {isOpen && (
           <CardContent>
             <p className="text-sm text-zinc-500">{project.description}</p>
